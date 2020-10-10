b8be07488c4e80196550b4c4b58f96dc8c323ab1
Mikami <Mikami9700@users.noreply.github.com>
2020-10-10 16:51:40 +0300

    Done Four

--- src/app.js
+++ src/app.js
@@ -61,7 +61,17 @@ app.post('/transfer', (req, res) => {
     res.render('transfer', { message: 'Transfer Complted' });
 });
 
-
+app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit
+}));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON,
+    'utf8');
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+    
+});
 
 app.get('/profile', (req, res) => {
     res.render('profile', { user: users[0] });
